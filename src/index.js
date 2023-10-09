e11cc3a6d8459ee0ab987157b85c7213fc180448
Gustavo Meireles <gugatpm@gmail.com>
2023-10-09 22:18:03 +0100

    installed slick-carousel and jquery

--- src/index.js
+++ src/index.js
@@ -1,56 +1,56 @@
-// element hover effect:
+// Element hover effect
 function elementHover(element, symbol) {
-  element.addEventListener("mouseenter", function() {
-    element.innerText = symbol + " " + element.innerText;
-  });
-
-  element.addEventListener("mouseleave", function() {
-    if (element.innerText.startsWith(symbol)) {
-      element.innerText = element.innerText.substring(symbol.length);
+  element.hover(
+    function() {
+      $(this).text(symbol + ' ' + $(this).text());
+    },
+    function() {
+      if ($(this).text().startsWith(symbol)) {
+        $(this).text($(this).text().substring(symbol.length));
+      }
     }
-  });
+  );
 }
 
+// Title hover effect
 function titleHover(element, symbol) {
-  element.addEventListener("mouseenter", function() {
-    element.innerText = symbol + " " + element.innerText + " " + symbol;
-  });
-
-  element.addEventListener("mouseleave", function() {
-    for (let i = 0; i < element.innerText.length; i++) {
-      element.innerText = element.innerText.replace("=", "");
+  element.hover(
+    function() {
+      $(this).text(symbol + ' ' + $(this).text() + ' ' + symbol);
+    },
+    function() {
+      $(this).text($(this).text().replace(/=/g, ''));
     }
-  });
+  );
 }
 
+// jQuery objects for elements
+let bullet = $('#bullet');
+let bio = $('#bio');
+let title = $('#title');
+let titleWeb = $('#title-web');
+let titleCalculator = $('#title-calculator');
+let titleDotnet = $('#title-dotnet');
+let titleFox = $('#title-fox');
 
-let bullet = document.getElementById("bullet");
-let bio = document.getElementById("bio");
-let title = document.getElementById("title");
-let titleWeb = document.getElementById("title-web");
-let titleCalculator = document.getElementById("title-calculator");
-let titleDotnet = document.getElementById("title-dotnet");
-let titleFox = document.getElementById("title-fox");
+// Title calls
+titleHover(title, '=');
 
-// titles
-titleHover(title, "=");
-elementHover(bio, "#");
-elementHover(titleWeb, "#");
-elementHover(titleCalculator, "##");
-elementHover(titleDotnet, "#");
-elementHover(titleFox, "##");
-
-// bullets
-elementHover(bullet, "*");
-
+// Bullet calls
+elementHover(bio, '#');
+elementHover(titleWeb, '#');
+elementHover(titleCalculator, '##');
+elementHover(titleDotnet, '#');
+elementHover(titleFox, '##');
+elementHover(bullet, '*');
 
 // Change bullet point every couple of seconds:
-const bulletPoints = ["Lover of programming.", "Linux Developer", "Web Developer"]
+const bulletPoints = ['Lover of programming.', 'Linux Developer', 'Web Developer'];
 let indexBulletPoint = 0;
 
 function updateBulletPoint() {
   if (indexBulletPoint < bulletPoints.length) {
-    bullet.innerText = bulletPoints[indexBulletPoint];
+    bullet.text(bulletPoints[indexBulletPoint]);
     indexBulletPoint++;
   } else {
     indexBulletPoint = 0;
