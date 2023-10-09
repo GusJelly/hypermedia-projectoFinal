32a017ea862190ad5056f4a05a6199b060fea235
Gustavo Meireles <gugatpm@gmail.com>
2023-10-09 10:22:49 +0100

    Stuff

--- src/index.js
+++ src/index.js
@@ -2,37 +2,35 @@ let bullet1 = document.getElementById("bullet1");
 let bullet2 = document.getElementById("bullet2");
 let bullet3 = document.getElementById("bullet3");
 
-console.log(bullet1.innerText);
-console.log(bullet2.innerText);
-console.log(bullet3.innerText);
+let title = document.getElementById("title");
 
-bullet1.addEventListener("mouseenter", function() {
-  bullet1.innerText = "* " + bullet1.innerText;
+//Function for title hover effect:
+title.addEventListener("mouseenter", function() {
+  title.innerText += "\n==============";
 });
+title.addEventListener("mouseleave", function() {
+  title.innerText = title.innerText.slice(0, -15);
+});
+// Functions for hover effect on bullet points:
+function hoverBullet(bullet) {
+  bullet.addEventListener("mouseenter", function () {
+    bullet.innerText = "* " + bullet.innerText;
+  });
+}
 
-bullet1.addEventListener("mouseleave", function() {
-  if (bullet1.innerText.startsWith("*")) {
-    bullet1.innerText = bullet1.innerText.substring(1);
-  }
-});
+function leaveBullet(bullet) {
+  bullet.addEventListener("mouseleave", function() {
+    if (bullet.innerText.startsWith("*")) {
+      bullet.innerText = bullet.innerText.substring(1);
+    }
+  });
+}
 
-bullet2.addEventListener("mouseenter", function() {
-  bullet2.innerText = "* " + bullet2.innerText;
-});
+hoverBullet(bullet1);
+hoverBullet(bullet2);
+hoverBullet(bullet3);
 
-bullet2.addEventListener("mouseleave", function() {
-  if (bullet2.innerText.startsWith("*")) {
-    bullet2.innerText = bullet2.innerText.substring(1);
-  }
-});
-
-bullet3.addEventListener("mouseenter", function() {
-  bullet3.innerText = "* " + bullet1.innerText;
-});
-
-bullet3.addEventListener("mouseleave", function() {
-  if (bullet3.innerText.startsWith("*")) {
-    bullet3.innerText = bullet3.innerText.substring(1);
-  }
-});
+leaveBullet(bullet1);
+leaveBullet(bullet2);
+leaveBullet(bullet3);
 
